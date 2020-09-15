3ff0503e276bd0551529428afbb1adf2af33813a
Казбеев Олег <kazbeev@gmail.com>
2020-09-15 14:53:52 +0700

    Todo_list views with CRUD functionality for task and board objects were created.

--- todo_app/todo_list/views.py
+++ todo_app/todo_list/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from .models import Board, Task
+from .serializers import BoardDetailSerializer, BoardListSerializer, \
+    TaskDetailSerializer, TaskListSerializer, TaskChangeSerializer
+
+
+class BoardCreateView(generics.CreateAPIView):
+    serializer_class = BoardDetailSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class BoardListView(generics.ListAPIView):
+    serializer_class = BoardListSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Board.objects.all()
+
+
+class BoardEditView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = BoardDetailSerializer
+    permission_classes = (IsAdminUser,)
+    lookup_field = 'id'
+    lookup_url_kwarg = 'board_id'
+
+
+class TaskCreateView(generics.CreateAPIView):
+    serializer_class = TaskDetailSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class TaskListView(generics.ListAPIView):
+    serializer_class = TaskListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Task.objects.filter(board=self.kwargs['board_id'])
+
+
+class TaskEditView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskChangeSerializer
+    permission_classes = (IsAuthenticated,)
+    lookup_field = 'id'
+    lookup_url_kwarg = 'task_id'
