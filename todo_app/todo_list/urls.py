50568abc80c3244b8bf30938536cc2a0f3775057
Казбеев Олег <kazbeev@gmail.com>
2020-09-16 14:35:36 +0700

    An mistakes were fixed.

--- todo_app/todo_list/urls.py
+++ todo_app/todo_list/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import BoardCreateView, BoardListSerializer, BoardEditView, \
+from .views import BoardCreateView, BoardListView, BoardEditView, \
     TaskCreateView, TaskListView, TaskEditView
 
 urlpatterns = [
     path('board/create/', BoardCreateView.as_view(), name='board_create'),
-    path('board/list/', BoardListSerializer.as_view(), name='board_list'),
+    path('board/list/', BoardListView.as_view(), name='board_list'),
     path('board/<int:board_id>/edit/', BoardEditView.as_view(), name='board_edit'),
     path('board/<int:board_id>/task/create', TaskCreateView.as_view(), name='task_create'),
     path('board/<int:board_id>/task/list/', TaskListView.as_view(), name='task_list'),
