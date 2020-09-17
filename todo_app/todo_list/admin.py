c34d6e6b69dfcdf17ed85fd4e2591bdb00da0fd2
Казбеев Олег <kazbeev@gmail.com>
2020-09-17 18:31:42 +0700

    Registration of models was added for django admin site.

--- todo_app/todo_list/admin.py
+++ todo_app/todo_list/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Board, Task, Organization
+
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Organization)
+
+
+
