c79048eb59168ac58389f180b31a9959edac1029
Казбеев Олег <kazbeev@gmail.com>
2020-09-17 19:30:53 +0700

    'organizations' field was added to field tuple. Also password write-only option was added with extra_kwargs.

--- todo_app/users/serializers.py
+++ todo_app/users/serializers.py
@@ -6,5 +6,6 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = ('id', 'email', 'first_name', 'last_name'
-                                              'password')
+        field = ('id', 'email', 'first_name', 'last_name',
+                 'organizations', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
