1002618acaa3ee11d67565a124fc217ac5eb5052
Казбеев Олег <kazbeev@gmail.com>
2020-09-17 17:55:04 +0700

    JWT-based authentication was added.

--- todo_app/users/views.py
+++ todo_app/users/views.py
@@ -1,8 +1,13 @@
-from rest_framework.permissions import AllowAny
-from rest_framework.views import APIView
-from rest_framework.response import Response
+import jwt
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_jwt.serializers import jwt_payload_handler
 
+from todo_app.settings import SECRET_KEY
+from .models import User
 from .serializers import UserSerializer
 
 
@@ -15,3 +20,33 @@ class CreateUserAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+    try:
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email, password=password)
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, SECRET_KEY)
+                user_details = {'name': "%s %s" % (
+                    user.first_name, user.last_name
+                ), 'token': token}
+                return Response(user_details, status=status.HTTP_200_OK)
+            except Exception as e:
+                raise e
+
+        else:
+            res = {
+                'error': 'Can not authenticate with the given credentials or the account has been deactivated'
+            }
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'Please provide an email and a password'}
+        return Response(res, status=status.HTTP_400_BAD_REQUEST)
+
