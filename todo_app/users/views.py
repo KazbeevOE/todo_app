97b2f7c9ec2e01b3c1f802111c887863e06552a9
Казбеев Олег <kazbeev@gmail.com>
2020-09-17 19:35:03 +0700

    Edit and Logout views were created.

--- todo_app/users/views.py
+++ todo_app/users/views.py
@@ -1,7 +1,9 @@
 import jwt
+from django.contrib.auth import user_logged_in
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_jwt.serializers import jwt_payload_handler
@@ -22,6 +24,33 @@ class CreateUserAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+class EditUserView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+
+        serializer = UserSerializer(
+            request.user, data=serializer_data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class LogoutUserView(APIView):
+    def get(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
 @api_view(['POST'])
 @permission_classes([AllowAny, ])
 def authenticate_user(request):
@@ -37,6 +66,8 @@ def authenticate_user(request):
                 user_details = {'name': "%s %s" % (
                     user.first_name, user.last_name
                 ), 'token': token}
+                user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
                 return Response(user_details, status=status.HTTP_200_OK)
             except Exception as e:
                 raise e
@@ -49,4 +80,3 @@ def authenticate_user(request):
     except KeyError:
         res = {'error': 'Please provide an email and a password'}
         return Response(res, status=status.HTTP_400_BAD_REQUEST)
-
