86e19b6cb584bd473ceb4a4a2d7daf3f09c9d7e6
Казбеев Олег <kazbeev@gmail.com>
2020-10-02 19:45:28 +0700

    Fixed EmployeeOnly permission.

--- todo_app/todo_list/permissions.py
+++ todo_app/todo_list/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework import permissions
 
+from todo_list.models import Board
+
 
 class EmployeeOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.board.organization in request.user.organizations
+    def has_permission(self, request, view):
+        board_id = request.resolver_match.kwargs.get('board_id')
+        board = Board.objects.get(id=board_id)
+        return board.organization in request.user.organizations.all()
